2d54015aa70a9b6232900eec905b933772a4e9ab
JoseMFT <jomaft03@gmail.com>
2022-11-16 01:21:19 +0000

    16/11/2022 - 1:21
    Código mejorado.
    Sigue sin funcionar.

--- Juego-Arrastrar-Cubo/Assets/Scripts/GameManager.cs
+++ Juego-Arrastrar-Cubo/Assets/Scripts/GameManager.cs
@@ -5,38 +5,30 @@ using TMPro;
 
 public class GameManager: MonoBehaviour {
 
-    int floating = 0;
+    bool floating = false;
 
     void Update () {
         // Si el número de veces clicado en pantalla es mayor o igual que uno Y se ha dejado de pulsar el clic izquierdo, o si se está pulsando el botón:
-        if (((Input.touchCount >= 1 && Input.GetTouch (0).phase == TouchPhase.Ended) || (Input.GetMouseButtonDown (0)))) { // (0 = botón izq., 1 = botón cent., 2= botón der.)
-            Vector2 mousePos = Input.mousePosition;
-
-            if (Application.platform == RuntimePlatform.Android) {
-                mousePos = Input.GetTouch (0).position;
-            }
-            Ray rayo = Camera.main.ScreenPointToRay (mousePos);
+        //if (((Input.touchCount >= 1 && Input.GetTouch (0).phase == TouchPhase.Began) || (Input.GetMouseButtonDown (0)))) { // (0 = botón izq., 1 = botón cent., 2= botón der.)
+        if (Input.GetMouseButtonDown (0)) {
+            Ray rayo = Camera.main.ScreenPointToRay (Input.mousePosition);
             RaycastHit hitInfo;
-            floating = (floating - 1) * -1; 
 
             if (Physics.Raycast (rayo, out hitInfo)) {
 
                 if (hitInfo.collider.tag.Equals ("Player")) {
+                    GameObject cubo = hitInfo.collider.gameObject;
+                    var posCubo = hitInfo.collider.GetComponent<Transform> ();
+                    cubo.SetActive (false);
+                    floating = true;
 
-                    if (floating >= 1) {
-                        GameObject cubo = hitInfo.collider.gameObject;
-                        Rigidbody rigidbodyCubo = cubo.GetComponent<Rigidbody> ();
-                        rigidbodyCubo.AddForce (new Vector3 (cubo.transform.position.x - mousePos.x, 0, cubo.transform.position.z - mousePos.y), ForceMode.Impulse);
-                        //cubo.transform.Translate (new Vector3 (mousePos.x / Screen.width, 0f, mousePos.y / Screen.height));
-                        cubo.SetActive (false);
-                        Debug.Log ("Posición cubo: " + cubo.transform.position);
+                    if (floating != false) {
                         cubo.SetActive (true);
+                        posCubo.position = hitInfo.point;
                     }
                 }
             }
         }
-
-        Debug.Log ("floating: " + floating);
     }    
 }
 
