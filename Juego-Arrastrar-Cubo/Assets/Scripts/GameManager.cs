79c02890e695f231338c914646b9f8fce3552eff
JoseMFT <jomaft03@gmail.com>
2022-11-16 21:12:06 +0000

    16/11/2022 - 21:11
    Hecho (?), varios fallos

--- Juego-Arrastrar-Cubo/Assets/Scripts/GameManager.cs
+++ Juego-Arrastrar-Cubo/Assets/Scripts/GameManager.cs
@@ -5,12 +5,16 @@ using TMPro;
 
 public class GameManager: MonoBehaviour {
 
-    bool floating = false;
+    bool floating = false; 
 
     void Update () {
         // Si el número de veces clicado en pantalla es mayor o igual que uno Y se ha dejado de pulsar el clic izquierdo, o si se está pulsando el botón:
         //if (((Input.touchCount >= 1 && Input.GetTouch (0).phase == TouchPhase.Began) || (Input.GetMouseButtonDown (0)))) { // (0 = botón izq., 1 = botón cent., 2= botón der.)
-        if (Input.GetMouseButtonDown (0)) {
+        if (Input.GetMouseButton (0)) {
+            clicking ();
+        }
+
+        void clicking () {
             Ray rayo = Camera.main.ScreenPointToRay (Input.mousePosition);
             RaycastHit hitInfo;
 
@@ -22,13 +26,22 @@ public class GameManager: MonoBehaviour {
                     cubo.SetActive (false);
                     floating = true;
 
-                    if (floating != false) {
-                        cubo.SetActive (true);
-                        posCubo.position = hitInfo.point;
-                    }
+                    if (hitInfo.collider.tag.Equals ("Player") || hitInfo.collider.tag.Equals ("Escenario")) {
+
+                        if (cubo.activeSelf == false) {
+                            Ray segundoRayo = Camera.main.ScreenPointToRay (Input.mousePosition);
+
+                            if (Physics.Raycast (segundoRayo, out hitInfo)) {
+
+                                if (floating == true) {
+                                    cubo.SetActive (true);
+                                    posCubo.position = hitInfo.point + new Vector3 (0f, 1f, -1f);
+                                }
+                            }
+                        } 
+                    }                    
                 }
             }
         }
     }    
 }
-
